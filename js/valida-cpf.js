167ada092b135d9aa361a6a55caef6f29cc927b6
Ana Karol <151869218+anakarolcatu@users.noreply.github.com>
2024-05-10 18:04:53 -0300

    valida cpf e idade adicionados

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,7 +1,9 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
-        campo.setCustomValidity('Esse cpf não é válido')
+    if(validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        console.log("Esse cpf não existe!");
+    } else {
+        console.log("Esse cpf existe!");
     }
 }
 
@@ -19,24 +21,25 @@ function validaNumerosRepetidos(cpf) {
         '99999999999'
     ]
 
-    return numerosRepetidos.includes(cpf)
+    return numerosRepetidos.includes(cpf);
 }
 
 function validaPrimeiroDigito(cpf) {
     let soma = 0;
     let multiplicador = 10;
 
-    for (let tamanho = 0; tamanho < 9; tamanho++) {
+    for(let tamanho = 0; tamanho < 9; tamanho++) {
         soma += cpf[tamanho] * multiplicador;
-        multiplicador--
+        multiplicador--;
     }
-
+    // soma todos os 9 primeiros dígitos e faz o módulo de 11
     soma = (soma * 10) % 11;
-
+    
+    // se o resultado for igual a 10 ou 11, coloca o valor da soma como 0
     if (soma == 10 || soma == 11) {
         soma = 0;
     }
-
+    //retorna se o valor da soma é igual ou diferente do nono dígito do cpf(se for igual, é válido)
     return soma != cpf[9];
 }
 
@@ -44,16 +47,17 @@ function validaSegundoDigito(cpf) {
     let soma = 0;
     let multiplicador = 11;
 
-    for (let tamanho = 0; tamanho < 10; tamanho++) {
+    for(let tamanho = 0; tamanho < 10; tamanho++) {
         soma += cpf[tamanho] * multiplicador;
-        multiplicador--
+        multiplicador--;
     }
-
+    // soma todos os 10 primeiros dígitos e faz o módulo de 11
     soma = (soma * 10) % 11;
-
+    
+    // se o resultado for igual a 10 ou 11, coloca o valor da soma como 0
     if (soma == 10 || soma == 11) {
         soma = 0;
     }
-
+    //retorna se o valor da soma é igual ou diferente do décimo dígito do cpf(se for igual, é válido)
     return soma != cpf[10];
 }
--- js/valida-idade.js
+++ js/valida-idade.js
@@ -1,13 +1,14 @@
 export default function ehMaiorDeIdade(campo) {
     const dataNascimento = new Date(campo.value);
-    if (!validaIdade(dataNascimento)) {
-        campo.setCustomValidity('O usuário não é maior de idade');
-    }
+    validaIdade(dataNascimento);
+
+    console.log(validaIdade(dataNascimento));
 }
 
 function validaIdade(data) {
     const dataAtual = new Date();
+    //pega a data de nascimento do campo e adiciona 18
     const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
-
+    //faz a comparação da data atual e da data mais 18 para saber se o usuário já fez 18 anos
     return dataAtual >= dataMais18;
 }
